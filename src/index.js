a710b15e65b55f3e1a9d4041ac33eedaa84df7d1
Andy McCullough <bigandy7@hotmail.com>
2017-07-30 19:05:28 +0100

    Webpack, babel working

---
+++ src/index.js
@@ -0,0 +1,15 @@
+class MyComp{    
+    constructor(){        
+    }
+    
+    getText(){
+        return 'my text again';
+    }
+}
+
+function component() {
+  var element = document.getElementById('root');  
+  element.innerHTML = new MyComp().getText();
+}
+
+component();
